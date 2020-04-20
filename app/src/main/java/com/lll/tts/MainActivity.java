3dd093dd189893323c9489a08e9e82a34906394e
erlei <erleizh@gmail.com>
2020-04-20 20:13:44 +0800

    修改gradle版本为4.6，设置默认引擎

--- app/src/main/java/com/lll/tts/MainActivity.java
+++ app/src/main/java/com/lll/tts/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lll.tts;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.lll.tts.utils.L;
 
@@ -20,18 +22,21 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     private TextToSpeech mSpeech;
     private AppCompatSpinner mSpinner;
     private EditText mInputText;
+    private TextView mTvInfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
-        mSpeech = new TextToSpeech(this, this, "com.svox.pico");
+        mSpeech = new TextToSpeech(this, this);
     }
 
     private void findViews() {
         mInputText = ((EditText) findViewById(R.id.et_input_text));
         mSpinner = ((AppCompatSpinner) findViewById(R.id.spinner));
+        mTvInfo = ((TextView) findViewById(R.id.tv_info));
     }
 
     public void play(View view) {
@@ -43,8 +48,22 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     @Override
     public void onInit(int status) {
         L.i("onInit status : %d", status);
-        if(status != TextToSpeech.ERROR) {
-            mSpeech.setLanguage(Locale.US);
+        if (status != TextToSpeech.ERROR) {
+            mSpeech.setLanguage(Locale.CHINA);
+            mSpeech.setEngineByPackageName(mSpeech.getDefaultEngine());
+            mTvInfo.append("DefaultEngine:");
+            mTvInfo.append(mSpeech.getDefaultEngine());
+            mTvInfo.append("\n");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                mTvInfo.append("AvailableLanguages:");
+                mTvInfo.append(mSpeech.getAvailableLanguages().toString());
+                mTvInfo.append("\n");
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                mTvInfo.append("DefaultVoice:");
+                mTvInfo.append(mSpeech.getDefaultVoice().toString());
+                mTvInfo.append("\n");
+            }
             initSpinner(mSpeech.getEngines());
         }
     }
@@ -56,16 +75,18 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
         mSpinner.setAdapter(adapter);
+        mSpinner.setSelection(arrayList.indexOf(mSpeech.getDefaultEngine()));
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 String name = arrayList.get(i);
                 mSpinner.setPrompt(name);
-                if(mSpeech.isSpeaking()){
+                if (mSpeech.isSpeaking()) {
                     mSpeech.stop();
                 }
                 mSpeech.setEngineByPackageName(name);
             }
+
             public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
